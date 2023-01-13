23366782284d81d3738236e91c47e952c655e336
Erika Miguel <erika@erikamiguel.com>
2023-01-13 11:58:52 -0500

    Adding publish status

--- classes/wordpress_handler.py
+++ classes/wordpress_handler.py
@@ -22,9 +22,13 @@ class WordpressHandler(Handler):
     def post_to_wordpress(self, content):
         metadata, html_content = self.parser.parse(content)
         data = {
+            "title": metadata["title"],
             "content": html_content,
-            "slug": metadata["slug"],
-            "title": metadata["title"]
+            "status": "publish",
+            "slug": metadata["slug"]
         }
-        response = requests.post(url=self.config['wordpress_url'], auth=(self.config['wordpress_auth_username'], self.config['wordpress_auth_password']), json=data, headers={'Content-Type': 'application/json'})
+        response = requests.post(url=self.config['wordpress_url'], 
+                                 auth=(self.config['wordpress_auth_username'], self.config['wordpress_auth_password']), 
+                                 json=data, 
+                                 headers={'Content-Type': 'application/json'})
         print(response)
